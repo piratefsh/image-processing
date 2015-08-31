3d5f6a205c0e97b0e8be4dfaba891c6e50233d29
Sher Minn Chong <sherminn.chong@gmail.com>
2015-08-31 15:12:41 -0400

    Add grayscale filter

--- sobel/js/main.js
+++ sobel/js/main.js
@@ -19,7 +19,24 @@ function init(){
         imgOffsetHeight = -(imgHeight - canvasDimensions.height)/2
         imgOffsetWidth = -(imgWidth - canvasDimensions.width)/2
         context.drawImage(img, imgOffsetWidth, imgOffsetHeight, imgWidth, imgHeight);
+        filterToGrayscale();
     }
+}
+
+function filterToGrayscale(){
+    // get image data (gives us pixels)
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
+    var data = imageData.data;
+
+    // loop through each pixel and set it to grey
+    for(var i = 0; i < data.length; i += 4){
+        // use luminosity formula to calculate brightness of grey
+        var r = data[i], g = data[i+1], b = data[i+2]; 
+        var luminosity = 0.21*r + 0.72*g + 0.07*b;
+        data[i] = data[i+1] = data[i+2] = luminosity;
+    }
+    // overwrite image
+    context.putImageData(imageData, 0, 0);
 }
 
 init();
