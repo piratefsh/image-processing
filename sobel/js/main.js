f62b75b3336881095eb07a5a9b0da994ce58ab28
Sher Minn Chong <sherminn.chong@gmail.com>
2015-08-31 18:34:34 -0400

    Fix serious bug with getting window

--- sobel/js/main.js
+++ sobel/js/main.js
@@ -12,9 +12,10 @@ function init(){
     var canvasDimensions = canvas.getBoundingClientRect();
     var img = new Image();
     
-    // img.src = 'img/noguchi01.jpg';
+    img.src = 'img/noguchi01.jpg';
     // img.src = 'img/coins.jpg';
-    img.src = 'img/phillylove.jpg';
+    // img.src = 'img/phillylove.jpg';
+    // img.src = 'img/concentriccircles.svg';
     img.onload = function(){
         imgWidth = canvasDimensions.width;
         imgHeight = this.height * canvasDimensions.width/this.width;
@@ -40,26 +41,6 @@ function getImageData(context){
     return arr;
 }
 
-// converts 2d array of pixels [[r,g,b,a],...[r,g,b,a]] to ImageData object
-function arrToImageData(arr){
-    var flatArr = [];
-    for(var i = 0; i < arr.length; i++){
-        if(!arr[i][4]){
-            flatArr.push(255)
-            flatArr.push(255)
-            flatArr.push(255)
-        }
-        else{
-            flatArr.push(0)
-            flatArr.push(0)
-            flatArr.push(0)
-        }
-        flatArr.push(255)
-    }
-
-    return new ImageData(new Uint8ClampedArray(flatArr), canvas.width);
-}
-
 function filterToGrayscale(){
     // get image data (gives us pixels)
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height)
@@ -76,14 +57,15 @@ function filterToGrayscale(){
     context.putImageData(imageData, 0, 0);
 }
 
-// convolutes 3x3 pixel window through Sobel kernel and returns processed value
+// convolutes 3x3 pixel window through Sobel kernel 
+// and returns if exceeds threshold
 function isEdge(w){
-    var threshold = 100;
-    var kernelX = [
+    var threshold = 80;
+    var kernelY = [
                 [-1, 0, 1], 
                 [-2, 0, 2],
                 [-1, 0, 1]];
-    var kernelY = [
+    var kernelX = [
                 [-1, -2, -1],
                 [0, 0, 0],
                 [1, 2, 1]];
@@ -97,24 +79,28 @@ function isEdge(w){
         }
     }
 
-    var magnitude = Math.sqrt(Math.pow(sumX, 2) + Math.pow(sumY, 2));
+    var magnitude = Math.sqrt(Math.ceil(Math.pow(sumX, 2) + Math.pow(sumY, 2)));
     
     // mark as 1 if it exceeds threshold
     return magnitude > threshold;
 }
 
 function edgeDetectSobel(){
-
-    var edgeColor = [0, 0, 0, 255]
+    var edgeColor = [255, 255, 0, 255]
     // for each pixel window, put it through Sobel kernel
     var data = getImageData(context);
     var edges = [];
 
-    for(var i = 0; i+8 < data.length; i+=1){
+    var maxPixelOffset = canvas.width*2+2;
+    var row2Offset = canvas.width
+    var row3Offset =  canvas.width*2
+    for(var i = 0; i + maxPixelOffset < data.length; i+=1){
+        var r2 = i + row2Offset;
+        var r3 = i + row3Offset;
         var pxWindow = [
             [data[i], data[i+1], data[i+2]],
-            [data[i+3], data[i+4], data[i+5]],
-            [data[i+6], data[i+7], data[i+8]],
+            [data[r2], data[r2+1], data[r2+2]],
+            [data[r3], data[r3+1], data[r3+2]],
             ]
         var edge = isEdge(pxWindow);
         if(edge){
@@ -122,7 +108,7 @@ function edgeDetectSobel(){
         }
         else{
             var transparentPix = data[i].slice();
-            transparentPix[3] = 125;
+            transparentPix[3] = 200;
             Array.prototype.push.apply(edges, transparentPix);
         }
     }
@@ -130,7 +116,7 @@ function edgeDetectSobel(){
     // pad missing edges
     var missing = data.length*data[0].length - edges.length
     for(var i = 0; i < missing/4; i++){
-        Array.prototype.unshift.apply(edges, [0, 0, 0, 255]);
+        Array.prototype.unshift.apply(edges, edgeColor);
     }
 
     // overwrite image
